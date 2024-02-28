f9e57262ae73faa8a27970a37bee5902c0b956f9
hitman4ik85 <hitman4ik85@gmail.com>
2024-02-28 20:01:26 +0200

    Add Form

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function sendRequest(url, method) {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// function sendRequest(url, method) {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// }
+
+// (function () {
+//   'use strict'
+
+//   var forms = document.querySelectorAll('.needs-validation')
+
+//   Array.prototype.slice.call(forms)
+//     .forEach(function (form) {
+//       form.addEventListener('submit', function (event) {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
+
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+// })()
+
+// function checkedValid(){
+//   const 
+// }
+
+function getFieldsFromForm(){
+  const fields = [
+    {
+      name: "string",
+      lastName: "string",
+      city: "sting",
+      age: Number || undefined,
+    },
+
+  ];
 }
