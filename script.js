026ee0ed67802cef9080b1f16136dd2891303b30
hitman4ik85 <hitman4ik85@gmail.com>
2024-02-28 21:06:17 +0200

    Add button

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function sendRequest(url, method) {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-}
-
-(function () {
-  'use strict'
-
-  var forms = document.querySelectorAll('.needs-validation')
-
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
-// function checkedValid(){
-//   const 
+// function sendRequest(url, method) {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
 // }
+
+// (function () {
+//   'use strict'
+
+//   var forms = document.querySelectorAll('.needs-validation')
+
+//   Array.prototype.slice.call(forms)
+//     .forEach(function (form) {
+//       form.addEventListener('submit', function (event) {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
+
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+// })()
+
+function checkedValid(){
+  const formContent = document.getElementById("form-extention")
+  
+}
 
 function getFieldsFromForm(){
   const fields = [
