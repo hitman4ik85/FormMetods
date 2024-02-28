4ca66474529dfed19070b511aafcd81907499458
hitman4ik85 <hitman4ik85@gmail.com>
2024-02-28 20:22:24 +0200

    Add Forms

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// function sendRequest(url, method) {
-//     fetch('https://jsonplaceholder.typicode.com/todos/1')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
-// }
+function sendRequest(url, method) {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
 
-// (function () {
-//   'use strict'
+(function () {
+  'use strict'
 
-//   var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.needs-validation')
 
-//   Array.prototype.slice.call(forms)
-//     .forEach(function (form) {
-//       form.addEventListener('submit', function (event) {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-// })()
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 // function checkedValid(){
 //   const 
